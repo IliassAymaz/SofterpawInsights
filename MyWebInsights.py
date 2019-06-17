78fce1fda649afefbb0fb0ae4c7a5de618005e64
aymaz.iliass@gmail.com <aymaz.iliass@gmail.com>
2019-06-17 20:56:41 +0200

    First commit

---
+++ MyWebInsights.py
@@ -0,0 +1,8 @@
+''' 
+Softerpaw aim behind creating MyWebUnsights is to be able to find out
+about the content and keywords that maximize the KPIs 
+(rankings, impressions, clicks, sales ...)
+
+The use cases that will make this happen in this context are:
++ 
+'''
