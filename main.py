4943d73921120ecfe1a78ce63dd73f19c6a0bf9d
IliassAymaz <aymaz.iliass@gmail.com>
2019-07-09 14:11:42 +0200

    set readme

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ from apiclient.discovery import build
 from oauth2client import OAuth2WebServerFlow
 
 
-# We want to be able to pull data from our search console
-# and generate insights for some questions
 
-# Examples are available on Github 
-# https://github.com/IliassAymaz/SofterpawInsights  # to be improved
+#
+
