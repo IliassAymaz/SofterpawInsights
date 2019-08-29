26c7f6689c9e49e78b06e4a72a95a4ee5c55a0f8
IliassAymaz <iliass@Asusification.localdomain>
2019-08-29 19:23:06 +0200

    switched to postgresql

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -79,13 +80,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        
+      
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'softerpawinsights',
+        'USER' : 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
+        
     }
 }
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# this overwrites DATABASES as written above!
+# DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
