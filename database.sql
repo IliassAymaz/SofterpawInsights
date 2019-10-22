bbb2e1971eb766e23d8c39048b4d9743b406aee7
IliassAymaz <iliass@Asusification.localdomain>
2019-10-22 18:59:48 +0200

    added database config

--- database.sql
+++ database.sql
@@ -8,7 +8,4 @@ keys VARCHAR,
 id serial not null primary key
 );
 insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-18', 0.0, 83.0, 0.0, 84.55421686746988) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-19', 0.0, 107.0, 0.0, 86.48598130841121) ;
+('2019-10-20', 0.0, 89.0, 0.0, 83.03370786516854) ;
