972ee5d9215ff6c65a41160db238bbf6b946f68d
IliassAymaz <iliass@Asusification.localdomain>
2019-10-22 03:51:44 +0200

    added database config

--- settings/production.py
+++ settings/production.py
@@ -11,3 +11,11 @@ DATABASES = {
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
+
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
