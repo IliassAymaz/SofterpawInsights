fe6ae2c5a8e9ffd2675950377a681dc561299322
IliassAymaz <aymaz.iliass@gmail.com>
2020-02-04 00:22:39 +0100

    fixed some database bugs

--- database.sql
+++ database.sql
@@ -8,58 +8,10 @@ keys VARCHAR,
 id serial not null primary key
 );
 insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-22', 0.0, 83.0, 0.0, 84.03614457831326) ;
+('2020-01-30', 0.0, 98.0, 0.0, 79.39795918367346) ;
 
 insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-23', 0.0, 75.0, 0.0, 83.36) ;
+('2020-01-31', 0.0, 68.0, 0.0, 83.16176470588235) ;
 
 insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-24', 0.0, 59.0, 0.0, 88.88135593220339) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-25', 0.0, 54.0, 0.0, 88.75925925925925) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-26', 0.0, 56.0, 0.0, 91.67857142857143) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-27', 0.0, 60.0, 0.0, 91.96666666666667) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-28', 0.0, 50.0, 0.0, 91.32) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-29', 0.0, 63.0, 0.0, 85.01587301587301) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-30', 0.0, 62.0, 0.0, 84.62903225806451) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-10-31', 0.0, 48.0, 0.0, 86.89583333333333) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-01', 0.0, 57.0, 0.0, 86.19298245614036) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-02', 0.0, 67.0, 0.0, 85.17910447761194) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-03', 0.0, 58.0, 0.0, 82.53448275862068) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-04', 0.0, 57.0, 0.0, 85.57894736842105) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-05', 0.0, 65.0, 0.0, 90.35384615384615) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-06', 0.0, 59.0, 0.0, 81.37288135593221) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-07', 0.0, 72.0, 0.0, 86.18055555555556) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-08', 0.0, 64.0, 0.0, 82.75) ;
-
-insert into performance(keys, clicks, impressions, ctr, position) values
-('2019-11-09', 0.0, 57.0, 0.0, 89.71929824561404) ;
+('2020-02-01', 1.0, 74.0, 0.013513513513513514, 81.52702702702703) ;
