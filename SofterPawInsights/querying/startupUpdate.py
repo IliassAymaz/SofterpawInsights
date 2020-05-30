d95146efe4016b11abfa2055b1ae1250cca110d4
Iliass <aymaz.iliass@gmail.com>
2020-05-30 11:28:47 +0200

    added test for sql execution

--- SofterPawInsights/querying/startupUpdate.py
+++ SofterPawInsights/querying/startupUpdate.py
@@ -32,7 +32,7 @@ class Updater:
             "./SofterPawInsights/querying/data/data_Available dates.json",
             "performance")
         sqlQuery, last_date_upstream = Converter.convert()
-        print(sqlQuery[:100])
+        print(sqlQuery)
         return sqlQuery, last_date_upstream
 
     def get_last_element_upstream(self):
